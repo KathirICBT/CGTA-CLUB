d567212c02aeef9674783cd0b1a9c4aa29896bec
Remnant <Remnantnew223@gmail.com>
2024-11-10 19:24:12 +0530

    Saai_10th_Migration_Order

--- database/migrations/2024_11_05_145203_create_notification_template_table.php
+++ database/migrations/2024_11_05_145203_create_notification_template_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notification_template', function (Blueprint $table) {
+        Schema::create('notification_templates', function (Blueprint $table) {
             $table->id();
             $table->string('type');
             $table->text('templateText');
