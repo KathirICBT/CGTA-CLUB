6e41eb0ca2f995118ae1505b672b69100d165c15
Remnant <Remnantnew223@gmail.com>
2024-12-07 13:59:25 +0530

    Saai_member_data_display

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
         @vite('resources/css/app.css')
         @livewireStyles
         <title>{{ $title ?? 'Page Title' }}</title>
