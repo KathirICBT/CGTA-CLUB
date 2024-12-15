4c0bf552a9aeb59ee8829823ca4391b40e533ea5
Remnant <Remnantnew223@gmail.com>
2024-12-15 16:56:25 +0530

    Saai_cards_aligned

--- resources/views/livewire/pages/member/member.blade.php
+++ resources/views/livewire/pages/member/member.blade.php
@@ -109,63 +109,76 @@
         @if(!$isTableView)
             <div id="card-view-container" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                 @foreach ($members as $member)
-                    <div class="px-6 py-6 text-center bg-white shadow-lg rounded-2xl lg:mt-0 xl:px-10 relative">
-                        <div class="space-y-4 xl:space-y-6">
-                            <div class="relative mx-auto h-36 w-36 rounded-full ring-2 ring-gray-400 p-0.5 flex justify-center items-center bg-white shadow-lg">
+                    <div class="px-6 py-6 text-center bg-white shadow-lg rounded-2xl relative lg:mt-0 xl:px-10">
+                        <div class="space-y-6 xl:space-y-8">
+                            <!-- Profile Image with Status Indicator -->
+                            <div class="relative mx-auto h-32 w-32 sm:h-36 sm:w-36 lg:h-40 lg:w-40 rounded-full ring-2 ring-gray-400 p-0.5 flex justify-center items-center bg-white shadow-lg">
                                 <img
-                                    class="rounded-full h-32 w-32 object-cover"
+                                    class="rounded-full h-28 w-28 sm:h-32 sm:w-32 lg:h-36 lg:w-36 object-cover"
                                     src="{{ $member['photo_url'] }}"
                                     alt="author avatar">
-
                                 <!-- Status Indicator -->
                                 <div
-                                    class="absolute top-2 right-6 transform translate-x-2 -translate-y-2 h-5 w-5 rounded-full ring-2 ring-white flex justify-center items-center
-                                    {{ $member['status'] === 'Active' ? 'bg-green-500' : '' }}
-                                    {{ $member['status'] === 'Inactive' ? 'bg-red-500' : '' }}
-                                    {{ $member['status'] === 'Waiting' ? 'bg-yellow-500' : '' }}">
+                                    class="absolute top-2 right-6 transform translate-x-2 -translate-y-2 h-5 w-5 sm:h-6 sm:w-6 rounded-full ring-2 ring-white flex justify-center items-center
+                                        {{ $member['status'] === 'Active' ? 'bg-green-500' : '' }}
+                                        {{ $member['status'] === 'Inactive' ? 'bg-red-500' : '' }}
+                                        {{ $member['status'] === 'Waiting' ? 'bg-yellow-500' : '' }}">
                                     <div
-                                        class="h-3 w-3 bg-white rounded-full ring-1 ring-white">
+                                        class="h-3 w-3 sm:h-4 sm:w-4 bg-white rounded-full ring-1 ring-white">
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="space-y-2">
-                                <div class="flex justify-center items-center flex-col space-y-7 text-lg font-medium leading-6">
-                                    <div class="">
-                                        <h3 class="text-gray-800 text-lg ">{{ $member['first_name'] }} {{ $member['last_name'] }}</h3>
-                                        <p class="text-gray-800 text-lg">Joined Since: {{ \Carbon\Carbon::parse($member['join_date'])->format('M j, Y') }}</p>
-                                        <div class="flex flex-col border border-black justify-start items-center space-x-6 mt-3 text-left">
-                                            <!-- Phone Icon -->
-                                            <a href="tel:{{ $member['phone'] }}" class="text-gray-500 hover:text-teal-500 flex items-center space-x-2">
-                                                <i class="fas fa-phone-alt text-lg"></i>
-                                                <span class="text-sm text-gray-800">{{ $member['phone'] }}</span>
-                                            </a>
-                                            <!-- Email Icon -->
-                                            <a href="mailto:{{ $member['email'] }}" class="text-gray-500 hover:text-teal-500 flex items-center space-x-2">
-                                                <i class="fas fa-envelope text-lg"></i>
-                                                <span class="text-sm text-gray-800">{{ $member['email'] }}</span>
-                                            </a>
-                                        </div>
+                            <!-- Member Info Section -->
+                            <div class="space-y-4 lg:space-y-6">
+                                <div class="flex flex-col justify-center items-center space-y-4 text-lg font-medium leading-6">
+                                    <h3 class="text-gray-800 text-lg sm:text-xl lg:text-2xl">
+                                        {{ $member['first_name'] }} {{ $member['last_name'] }}
+                                    </h3>
+                                    <p class="text-gray-800 text-sm sm:text-base lg:text-lg">
+                                        Joined Since: {{ \Carbon\Carbon::parse($member['join_date'])->format('M j, Y') }}
+                                    </p>
+                                    <p class="text-gray-800 text-sm sm:text-base lg:text-lg">
+                                        Renewed Date: {{ \Carbon\Carbon::parse($member['renewal_date'])->format('M j, Y') }}
+                                    </p>
+                                    <!-- Contact Details -->
+                                    <div class="flex flex-col justify-start items-center space-y-3 lg:space-y-4 mt-3 text-left">
+                                        <!-- Phone Icon -->
+                                        <button
+                                            class="text-teal-500 hover:text-teal-700 flex items-center space-x-2"
+                                            onclick="copyToClipboard('{{ $member['phone'] }}')">
+                                            <i class="fas fa-phone-alt text-lg sm:text-xl"></i>
+                                            <span class="text-sm sm:text-base lg:text-lg text-gray-800">{{ $member['phone'] }}</span>
+                                        </button>
+
+                                        <!-- Email Icon -->
+                                        <button
+                                            class="text-teal-500 hover:text-teal-700 flex items-center space-x-2"
+                                            onclick="copyToClipboard('{{ $member['email'] }}')">
+                                            <i class="fas fa-envelope text-lg sm:text-xl"></i>
+                                            <span class="text-sm sm:text-base lg:text-lg text-gray-800">{{ $member['email'] }}</span>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-col justify-center items-center mt-5 space-y-1 absolute -top-4 right-0 mr-3">
-                            <!-- Twitter Icon -->
+
+                        <!-- Action Buttons Section -->
+                        <div class="flex flex-col justify-center space-y-2 lg:space-y-4 mt-5 absolute -top-4 right-0 mr-3 sm:mr-6">
+                            <!-- Edit Button -->
                             <button
-                               class="text-amber-900 hover:bg-amber-300 text-lg  w-10 h-10 flex justify-center items-center p-4  rounded-full bg-amber-200"
-                               wire:click="editMember({{ $member['id'] }},  'form')">
-                                <i class="fas fa-edit text-gray-500  pl-1"></i>
+                                class="text-amber-900 hover:bg-amber-300 text-base sm:text-lg w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center rounded-full bg-amber-200">
+                                <i class="fas fa-edit text-gray-500"></i>
                             </button>
+                            <!-- Delete Button -->
                             <button
-                               class="text-rose-900 text-lg hover:bg-rose-300  w-10 h-10 flex justify-center items-center p-4 rounded-full bg-rose-200"
-                               wire:click="deleteMember({{ $member['id'] }})">
-                                <i class="fas fa-trash-alt  pt-1"></i>
+                                class="text-rose-900 hover:bg-rose-300 text-base sm:text-lg w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center rounded-full bg-rose-200">
+                                <i class="fas fa-trash-alt"></i>
                             </button>
+                            <!-- Info Button -->
                             <button
-                                class="text-sky-900 text-lg hover:bg-sky-300  w-10 h-10 flex justify-center items-center p-4 rounded-full bg-sky-200"
-                                wire:click="editMember({{ $member['id'] }}, 'view')">
-                                <i class="fas fa-info-circle  pt-1"></i>
+                                class="text-sky-900 hover:bg-sky-300 text-base sm:text-lg w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center rounded-full bg-sky-200">
+                                <i class="fas fa-info-circle"></i>
                             </button>
                         </div>
                     </div>
@@ -183,6 +196,16 @@
         @endif
     </div>
 </div>
+
+<script>
+    function copyToClipboard(data) {
+        navigator.clipboard.writeText(data).then(() => {
+            alert("Copied to clipboard: " + data);
+        }).catch(err => {
+            console.error("Could not copy text: ", err);
+        });
+    }
+</script>
 
 
 
